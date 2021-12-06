0257d1083131be983f5db05157cd2956168b609d
Justin Wood <jwood@starry.com>
2021-12-06 09:49:23 -0500

    Day 5 - Part 1

---
+++ py-aoc/py_aoc/day5.py
@@ -0,0 +1,126 @@
+"""day4 module"""
+import os
+
+from typing import Any, List
+
+# pylint: disable=invalid-name
+inputfile = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "../../days/5/input.txt")
+)
+
+
+class Point:
+    """A single point"""
+
+    x: int
+    y: int
+
+    def __init__(self, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
+
+    @classmethod
+    def from_data(cls, data: str) -> "Point":
+        """Parse a Point from data"""
+        x, y = (int(p) for p in data.strip().split(","))
+        return Point(x, y)
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, Point):
+            return False
+        return self.x == other.x and self.y == other.y
+
+    def __repr__(self) -> str:
+        return f"Point<{self.x}, {self.y}>"
+
+
+class Line:
+    """Represent a line"""
+
+    start: Point
+    end: Point
+
+    def __init__(self, start: Point, end: Point) -> None:
+        self.start = start
+        self.end = end
+
+    @classmethod
+    def from_data(cls, data: str) -> "Line":
+        """Parse a Line from data"""
+        start_data, end_data = (edge for edge in data.strip().split("->"))
+        start = Point.from_data(start_data)
+        end = Point.from_data(end_data)
+        return Line(start, end)
+
+    def is_angled(self) -> bool:
+        """Returns False if this line is vertical or horizontal"""
+        if self.start.x == self.end.x or self.start.y == self.end.y:
+            return False
+        return True
+
+    def max_x(self) -> int:
+        """Return the max x coord"""
+        return max(self.start.x, self.end.x)
+
+    def max_y(self) -> int:
+        """Return the max y coord"""
+        return max(self.start.y, self.end.y)
+
+    def walk(self) -> List[Point]:
+        """Returns a list of points along the line
+
+        Assumes horizontal/vertical"""
+        walking = []
+        step = 1
+        if self.start.x == self.end.x:
+            if self.start.y > self.end.y:
+                step = -1
+            for y_walk in range(self.start.y, self.end.y + step, step):
+                walking.append(Point(self.start.x, y_walk))
+        if self.start.y == self.end.y:
+            if self.start.x > self.end.x:
+                step = -1
+            for x_walk in range(self.start.x, self.end.x + step, step):
+                walking.append(Point(x_walk, self.start.y))
+        return walking
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, Line):
+            return False
+        return self.start == other.start and self.end == other.end
+
+    def __repr__(self) -> str:
+        return f"Line<start={self.start}, end={self.end}>"
+
+
+def get_lines(data: str) -> List[Line]:
+    """Parse data to get the number of Lines"""
+    lines: List[Line] = []
+    for line in data.splitlines():
+        lines.append(Line.from_data(line))
+    return lines
+
+
+def part1(lines: List[Line]) -> int:
+    """Compute Part 1"""
+    max_x = max(line.max_x() for line in lines)
+    max_y = max(line.max_y() for line in lines)
+    grid = [[0 for _ in range(max_x + 1)] for _ in range(max_y + 1)]
+
+    # Mark the grid
+    for line in lines:
+        if line.is_angled():
+            continue
+        for point in line.walk():
+            grid[point.y][point.x] += 1
+    total = sum(sum(coord >= 2 for coord in l) for l in grid)
+    return total
+
+
+def main() -> None:
+    """Main Logic"""
+    with open(inputfile) as infile:
+        lines = get_lines(infile.read())
+
+    print("Part 1:", part1(lines))
+    # print("Part 2:", part2(calls, cards))
---
+++ py-aoc/tests/test_day5.py
@@ -0,0 +1,58 @@
+"""Tests for day 5"""
+from py_aoc import day5
+
+from pytest import mark
+
+
+EXAMPLE_DATA = """0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2
+"""
+
+
+@mark.parametrize(
+    "data,expect",
+    (
+        ["12,6", day5.Point(12, 6)],
+        ["896,7", day5.Point(896, 7)],
+        ["999,999", day5.Point(999, 999)],
+    ),
+)
+def test_point(data: str, expect: day5.Point) -> None:
+    """Test point data"""
+    point = day5.Point.from_data(data)
+    assert point == expect
+
+
+@mark.parametrize(
+    "data,expect,angled",
+    (
+        ["0,9 -> 5,9", day5.Line(day5.Point(0, 9), day5.Point(5, 9)), False],
+        ["9,4 -> 3,4", day5.Line(day5.Point(9, 4), day5.Point(3, 4)), False],
+        ["6,4 -> 2,0", day5.Line(day5.Point(6, 4), day5.Point(2, 0)), True],
+    ),
+)
+def test_line(data: str, expect: day5.Point, angled: bool) -> None:
+    """Test line data"""
+    line = day5.Line.from_data(data)
+    assert line == expect
+    assert line.is_angled() == angled
+
+
+def test_data_extract() -> None:
+    """Test data extraction"""
+    lines = day5.get_lines(EXAMPLE_DATA)
+    assert all(isinstance(line, day5.Line) for line in lines)
+
+
+def test_part1() -> None:
+    """Test part 1"""
+    lines = day5.get_lines(EXAMPLE_DATA)
+    assert day5.part1(lines) == 5
